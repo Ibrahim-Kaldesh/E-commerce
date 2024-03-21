7aec890d87e0625f8403da3baf70c7fc9e36ce43
Ibrahim-Kaldesh <ebrahim195011@feng.bu.edu.eg>
2024-03-21 23:28:40 +0200

    Some of Controller functions identifiers has been added

--- Backend/Controllers/ProductControllers/productControllers.js
+++ Backend/Controllers/ProductControllers/productControllers.js
@@ -2,3 +2,11 @@ import {
   readFromJson,
   writeToJson,
 } from "../../data/HandleR&W/readAndWrite.js";
+
+const products = JSON.parse(readFromJson("./data/products.json"));
+
+export const showAllProducts = function (req, res, next) {};
+export const createProduct = function (req, res, next) {};
+export const removeProduct = function (req, res, next) {};
+export const showProductById = function (req, res, next) {};
+export const updateProductById = function (req, res, next) {};
--- Backend/Controllers/UserControllers/userControllers.js
+++ Backend/Controllers/UserControllers/userControllers.js
@@ -2,3 +2,10 @@ import {
   readFromJson,
   writeToJson,
 } from "../../data/HandleR&W/readAndWrite.js";
+
+const users = JSON.parse(readFromJson("./data/users.json"));
+
+export const showAllUsers = function (req, res, next) {};
+export const createUser = function (req, res, next) {};
+export const removeUser = function (req, res, next) {};
+export const showUserById = function (req, res, next) {};

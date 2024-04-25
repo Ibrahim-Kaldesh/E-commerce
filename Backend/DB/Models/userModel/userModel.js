2b401aaa47e907d59a31663777063d11200d187c
Ibrahim-Kaldesh <ebrahim195011@feng.bu.edu.eg>
2024-04-25 23:08:05 +0200

    Document Middleware has been enhanced with adding some check

--- Backend/DB/Models/userModel/userModel.js
+++ Backend/DB/Models/userModel/userModel.js
@@ -91,9 +91,20 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+// userSchema.pre(/^find/, function (next) {
+//   this.populate({
+//     path: "books",
+//     select: "-users",
+//   });
+
+//   next();
+// });
+
 userSchema.pre("save", async function (next) {
-  this.password = await bcrypt.hash(this.password, 12);
-  this.passwordConfirm = this.password;
+  if (this.isNew) {
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirm = this.password;
+  }
   next();
 });
 

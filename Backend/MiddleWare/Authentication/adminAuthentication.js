22e488c21b110757f8c65458d989eefbd809d46c
ahmedabosalem99 <ahmed195043@feng.bu.edu.eg>
2024-04-30 23:08:24 +0300

    Admin Authentication

---
+++ Backend/MiddleWare/Authentication/adminAuthentication.js
@@ -0,0 +1,9 @@
+import AppError from "../../util/appError.js";
+
+export const admiAuth = function (...roles) {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role))
+      return next(new AppError(401, "unauthorized user !!"));
+    next();
+  };
+};

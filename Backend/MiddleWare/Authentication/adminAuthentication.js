9833b2d0fd759c084e8f7d71b70dd5c29fa70a72
Ibrahim-Kaldesh <ebrahim195011@feng.bu.edu.eg>
2024-05-01 04:23:02 +0300

    update admin

--- Backend/MiddleWare/Authentication/adminAuthentication.js
+++ Backend/MiddleWare/Authentication/adminAuthentication.js
@@ -4,6 +4,6 @@ export const admiAuth = function (...roles) {
   return (req, res, next) => {
     if (!roles.includes(req.user.role))
       return next(new AppError(401, "unauthorized user !!"));
-    next();
-  };
+    next();
+  };
 };

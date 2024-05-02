4d72968739f1e1a372bdb457a121b7ea5f737b9b
Mahmoud Hawara <154727577+Mahmoud-Hawara@users.noreply.github.com>
2024-05-02 16:19:16 +0300

    fix helper's search

--- Backend/helpers/searchById.js
+++
@@ -1,7 +0,0 @@
-export const finduserById = function (users, req) {
-  return users.find((u) => u.id === +req.params.userId);
-};
-
-export const findBookById = function (books, req) {
-  return books.find((b) => b.id === +req.params.bookId);
-};
